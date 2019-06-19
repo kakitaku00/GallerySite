b3be8ea602f6d1254a7b20749daeee3f54daf34f
柿沼 拓実 <takumi_kakinuma@waku-2.com>
2019-06-19 19:46:27 +0900

    fix pager

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ $(function() {
   const flickrServer = "https://api.flickr.com/services/rest";
   const api_key = "&api_key=ec560fe4b5414933f7dd557ed0ec7a78";
   const flickerMethod = "?method=flickr.photos.search";
-  const flickrFrormat = "&format=json&nojsoncallback=1&extras=url_sq,date_taken"
+  const flickrFrormat = "&format=json&nojsoncallback=1&extras=url_q,date_taken"
+  const textFormat = "&text="
   let searchValue = "";
-  const per_page = "&per_page=50";
+  const per_page = "&per_page=50"; // 1ページ50枚表示
   const page = "&page=";
-  let pageNum = 1
+  let pageNum = 0
 
   // 検索
   searchBtn.on("click", function() {
     // 検索文字が同じ時は処理をしない
     if (searchValue === searchText[0].value) {return}
     searchValue = searchText[0].value;
+    pageNum = 1
     // Gallery/Pagerの中を空にする
     resetGallery()
+    pager.empty();
     // 画像を取得
-    const searchURI = flickrServer + flickerMethod + api_key + flickrFrormat + "&text=" + searchValue + per_page + page + pageNum;
+    const searchURI = flickrServer + flickerMethod + api_key + flickrFrormat + textFormat + searchValue + per_page + page + pageNum;
     getData(searchURI);
   })
 
@@ -37,47 +40,80 @@ $(function() {
       dataType: 'json'
     })
     .then(function(data) {
+      console.log(data)
       // 写真の枚数
-      const viewPhotoNum = data.photos.perpage;
-      // 枚数分取得
-      for (let imageNum = 0; imageNum < viewPhotoNum; imageNum++) {
-        const photo = data.photos.photo[imageNum];
-        const farmId = photo.farm;
-        const server = photo.server;
-        const id = photo.id;
-        const secret = photo.secret;
-        const imageWidth = photo.width_sq;
-        const imageHeight = photo.height_sq;
-        const srcUrl = "http://farm" + farmId + ".staticflickr.com/"+ server +"/" + id + "_"+ secret +".jpg";
-        const image = new Image(imageHeight, imageWidth);
-        image.src = srcUrl;
-        gallery.append(image);
-      }
+      const viewPhotoNum = data.photos.photo.length;
+      const pages = data.photos.pages
+      const currentPages = data.photos.page
+      // 枚数分描画
+      viewPhoto(data, viewPhotoNum);
+      viewpager(currentPages, pages);
     })
     .then(function() {
-      $(".Pager__anchor:first").addClass("is-active");
       $(".Pager__anchor").each(function() {
         // イベントリスナーがすでに登録されていいた場合はスルー
-        if (jQuery._data($(this).get(0)).events) {
-          return
-        }
-        pageNum = $(this)[0].getAttribute("id")
+        if (jQuery._data($(this).get(0)).events) {return}
         $(this).on("click", function(e) {
           e.preventDefault();
-          const targetPageNum = $(this)[0].getAttribute("id");
-          if (pageNum !== targetPageNum) {
-            pageNum = targetPageNum;
-            resetGallery()
-            $(this).addClass("is-active");
-            const search = flickrServer + flickerMethod + api_key + flickrFrormat + "&text=" + searchValue + per_page + page + pageNum;
-            getData(search);
-          }
+          pager.empty();
+          pageNum = $(this)[0].getAttribute("id");
+          resetGallery()
+          $(this).addClass("is-active");
+          const search = flickrServer + flickerMethod + api_key + flickrFrormat + textFormat + searchValue + per_page + page + pageNum;
+          getData(search);
         })
       })
     })
   }
+
   function resetGallery() {
     gallery.empty();
     $(".is-active").removeClass("is-active");
+  }
+
+  function viewPhoto(data, viewPhotoNum) {
+    for (let imageNum = 0; imageNum < viewPhotoNum; imageNum++) {
+      const photo = data.photos.photo[imageNum];
+      const farmId = photo.farm;
+      const server = photo.server;
+      const id = photo.id;
+      const secret = photo.secret;
+      const imageWidth = photo.width_q;
+      const imageHeight = photo.height_q;
+      const srcUrl = "http://farm" + farmId + ".staticflickr.com/"+ server +"/" + id + "_"+ secret +".jpg";
+      const image = new Image(imageHeight, imageWidth);
+      image.src = srcUrl;
+      gallery.append(image);
+    }
+  }
+
+  function viewpager(currentPage, maxPages) {
+    // ページャーを表示するブロックの最大数
+    let viewPagerBlock = 3
+    // 取得したmaxPagesがブロックの最大数以下の場合更新
+    if (maxPages < viewPagerBlock) {
+      viewPagerBlock = maxPages;
+    }
+    // 2ページを表示した際に[1,2,3]と表示させたい
+    let currentMaxViewPager = currentPage + viewPagerBlock - 2;
+    // 調整用
+    let adjustNum = -1;
+    // 最初のページ
+    if (currentPage === 1) {
+      adjustNum++;
+      currentMaxViewPager++;
+    }
+    // 最後のページ
+    if (currentPage === maxPages || maxPages !== 2) {
+      adjustNum--;
+      currentMaxViewPager--;
+    }
+
+    for (let i = currentPage + adjustNum; i <= currentMaxViewPager; i++) {
+      pager.append('<li class="Pager__list"><a href="#' + i + '" id="' + i + '" class="Pager__anchor">' + i + '</a></li>')
+      if (i === currentPage) {
+        $("#" + currentPage).addClass("is-active");
+      }
+    }
   }
 })
